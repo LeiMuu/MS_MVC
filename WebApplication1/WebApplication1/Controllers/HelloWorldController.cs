c3d99458773aa384941b283b801069557ebf17f3
LeiMuu <hugo83101@gmail.com>
2019-08-27 18:07:06 +0800

    範例1-4 新增view-HellowWorld/Index.cshtml

--- WebApplication1/WebApplication1/Controllers/HelloWorldController.cs
+++ WebApplication1/WebApplication1/Controllers/HelloWorldController.cs
@@ -9,14 +9,9 @@ namespace WebApplication1.Controllers
     public class HelloWorldController : Controller
     {
         // GET: /HelloWorld/
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
-        }
-        // GET: /HelloWorld/Welcome/ 
-        public string Welcome(string name, int ID = 1)
-        {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            return View();
         }
     }
 }
