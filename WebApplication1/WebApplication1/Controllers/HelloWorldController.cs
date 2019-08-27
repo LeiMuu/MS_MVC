784e2bcfae5390791846f4f9edb0513775d7c1c1
LeiMuu <hugo83101@gmail.com>
2019-08-27 17:26:43 +0800

    範例1-3添加Hello的url路由規則

--- WebApplication1/WebApplication1/Controllers/HelloWorldController.cs
+++ WebApplication1/WebApplication1/Controllers/HelloWorldController.cs
@@ -14,9 +14,9 @@ namespace WebApplication1.Controllers
             return "This is my <b>default</b> action...";
         }
         // GET: /HelloWorld/Welcome/ 
-        public string Welcome(string name, int numTimes = 1)
+        public string Welcome(string name, int ID = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name+ ", NumTime is: "+ numTimes);
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
         }
     }
 }
